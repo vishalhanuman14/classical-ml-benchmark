6b1ff39eafa2b734852ec22be8dccabf5e31fe84
Alola420 <125461218+Alola420@users.noreply.github.com>
2026-05-07 04:08:51 -0400

    Revert
    Updated GaussianNaiveBayes to use dynamic epsilon for standard deviation calculation.

--- algorithms/akshit/hw_2_naive_bayes.py
+++ algorithms/akshit/hw_2_naive_bayes.py
@@ -2,26 +2,21 @@ import numpy as np
 import math
 
 class GaussianNaiveBayes:
-    """
-    Gaussian Naive Bayes classifier for continuous features.
-    """
-    def __init__(self):
+    def __init__(self, epsilon=1e-9):
         self.classes = None
         self.priors = {}
         self.means = {}
         self.stds = {}
+        self.epsilon = epsilon
 
     def fit(self, X_train, y_train):
         self.classes = np.unique(y_train)
         n = len(y_train)
-        
         for c in self.classes:
             mask = y_train == c
             self.priors[c] = np.sum(mask) / n
             self.means[c] = np.mean(X_train[mask], axis=0)
-            # Add the dynamic epsilon instead of a hardcoded 1e-9
             self.stds[c]  = np.std(X_train[mask], axis=0) + self.epsilon
-            
         return self
 
     def _log_likelihood(self, x, mean, std):
@@ -30,7 +25,6 @@ class GaussianNaiveBayes:
     def _predict_one(self, x):
         best_class, best_score = None, float('-inf')
         for c in self.classes:
-            # Sum the log-likelihoods of all features for this class
             score = math.log(self.priors[c]) + float(np.sum(self._log_likelihood(x, self.means[c], self.stds[c])))
             if score > best_score:
                 best_score, best_class = score, c
